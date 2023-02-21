6d736249b82416c68029816de8fa221eb9bb7282
Ryan Milligan <ryanmilligan88@gmail.com>
2023-02-20 16:56:48 -0800

    Added Class for Tracing the Apparent Retrograde Motion of one BOdy relative to another

---
+++ Source/Orbit_Simulation/Private/RetrogradePath.cpp
@@ -0,0 +1,225 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RetrogradePath.h"
+#include "AstroBody.h"
+#include "Sim.h"
+#include "ToolBuilderUtil.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+ARetrogradePath::ARetrogradePath() :
+ReferenceBody(nullptr),
+OtherBody(nullptr),
+Duration(3650.0),
+Interval(5),
+ForwardAxis(ESplineMeshAxis::Z),
+SplinePointCount(0),
+SplineMeshCount(0)
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Get reference to Sim
+	TArray<AActor*> SimArray;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ASim::StaticClass(), SimArray);
+	if (SimArray.Num() > 0)
+	{
+		Sim = Cast<ASim>(SimArray[0]);
+	}
+
+	// Create Scene Root Component
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Scene_Root"));
+	SetRootComponent(SceneRoot);
+
+	// Create Spline Component
+	SplineComponent = CreateDefaultSubobject<USplineComponent>(TEXT("Spline"));
+	if (SplineComponent)
+	{
+		SplineComponent->SetupAttachment(SceneRoot);
+		SplineComponent->SetRelativeLocation(FVector::ZeroVector);
+		SplineComponent->SetCastShadow(false); // Disable Shadows
+		SplineComponent->SetDrawDebug(false);
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Error Creating Spline Component"));
+	}
+
+	// Initialize Mesh
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> CylinderMeshAsset(TEXT("StaticMesh'/Engine/BasicShapes/Cylinder.Cylinder'"));
+	if (CylinderMeshAsset.Succeeded())
+	{
+		Mesh = CylinderMeshAsset.Object;
+	}
+	
+	// Initialize Material
+	ConstructorHelpers::FObjectFinder<UMaterialInterface> Mat(TEXT("/Engine/VREditor/UI/KeyDiskMaterial.KeyDiskMaterial"));
+	if (Mat.Succeeded())
+	{
+		DefaultMaterial = Mat.Object;
+	}
+	
+	SplineComponent->ClearSplinePoints();
+	SplineMeshes.Empty();
+}
+
+void ARetrogradePath::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	//ForwardAxis = ESplineMeshAxis::Z;
+	double size = 0.2;
+	SetActorScale3D(FVector(size, size, size));
+
+	if(!ReferenceBody) return;
+
+	// Attach to Reference Body
+	SetActorLocation(ReferenceBody->GetActorLocation());
+	AttachToActor(ReferenceBody, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+
+	
+	SplineComponent->SetClosedLoop(false);
+}
+
+// Called when the game starts or when spawned
+void ARetrogradePath::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Timer = 0.0; // Reset Timer
+	SplineComponent->SetWorldLocation(ReferenceBody->GetActorLocation());
+
+	// Create first two points
+	InitializeFirstPoints();
+
+	// Reset timer since update
+	TimeSinceLastUpdate = 0.0;
+}
+
+void ARetrogradePath::InitializeFirstPoints()
+{
+	if (!OtherBody || !SplineComponent || !Mesh) return;
+
+	// Add two initial points and one Spline Mesh between them
+	AddNewPoint();
+	AddNewPoint();
+	AddSplineMesh();
+	UpdateMesh();
+	
+}
+
+void ARetrogradePath::AddSplineMesh()
+{
+	// Add Spline Mesh Component
+	USplineMeshComponent* SplineMeshComponent = NewObject<USplineMeshComponent>(this, USplineMeshComponent::StaticClass());
+	SplineMeshComponent->SetMobility(EComponentMobility::Movable); // Set Mobility
+	SplineMeshComponent->SetStaticMesh(Mesh);
+	SplineMeshComponent->CreationMethod = EComponentCreationMethod::UserConstructionScript;
+	SplineMeshComponent->RegisterComponentWithWorld(GetWorld());
+	SplineMeshComponent->AttachToComponent(SplineComponent, FAttachmentTransformRules::KeepRelativeTransform);
+
+	SplineMeshComponent->SetForwardAxis(ForwardAxis);
+
+	if (DefaultMaterial) SplineMeshComponent->SetMaterial(0, DefaultMaterial); // Set material
+
+	SplineMeshComponent->SetCastShadow(false); // Disable Shadows
+	SplineMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	SplineMeshComponent->SetVisibility(true);
+	SplineMeshComponent->SetHiddenInGame(false);
+
+	SplineMeshes.Add(SplineMeshComponent); // Add Spline Mesh to Array
+
+	// Update counter
+	SplineMeshCount = SplineMeshes.Num();
+}
+
+void ARetrogradePath::UpdateMesh()
+{
+	int NumOfPoints = SplineComponent->GetNumberOfSplinePoints();
+	int NumOfMeshes = SplineMeshes.Num();
+	
+	for(int i = 0; i < NumOfMeshes && i < NumOfPoints - 1; i++)
+	{
+		// Get Start Point
+		const FVector StartPoint = SplineComponent->GetLocationAtSplinePoint(i, ESplineCoordinateSpace::Local);
+		const FVector StartTangent = SplineComponent->GetTangentAtSplinePoint(i, ESplineCoordinateSpace::Local);
+
+		// Get End Point
+		const FVector EndPoint = SplineComponent->GetLocationAtSplinePoint(i + 1, ESplineCoordinateSpace::Local);
+		const FVector EndTangent = SplineComponent->GetTangentAtSplinePoint(i + 1, ESplineCoordinateSpace::Local);
+
+		// Update Spline Mesh
+		SplineMeshes.Last()->SetStartAndEnd(StartPoint, StartTangent, EndPoint, EndTangent, true);
+	}
+}
+
+// Called every frame
+void ARetrogradePath::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if(!Sim) return;
+	double Delta = DeltaTime * Sim->TimeScale;
+	// Update timers
+	Timer += Delta;
+	TimeSinceLastUpdate += Delta;
+
+	// Rather than simply adding a new point every interval, which would look very abrupt,
+	// we can update just the position of the last point each tick, and add a new point
+	// at each interval. This way we can have longer intervals, and need to have less points overall
+
+	// If not enough time has passed between updates, update position of last Spline Point
+	if(TimeSinceLastUpdate < Interval)
+	{
+		UpdateLastPoint();
+	}
+	else
+	{
+		AddNewPoint();
+		AddSplineMesh();
+		TimeSinceLastUpdate = 0.0;
+	}
+	
+	UpdateMesh();
+}
+
+void ARetrogradePath::UpdateLastPoint()
+{
+	if(!OtherBody || !SplineComponent || !Mesh) return;
+	if(SplineComponent->GetNumberOfSplinePoints() < 2) return;
+	
+	// Get position of other body
+	FVector Position = OtherBody->GetActorLocation();
+
+	// Get index of last Spline Point
+	int index = SplineComponent->GetNumberOfSplinePoints() - 1;
+	
+	// Update position of last Spline Point
+	SplineComponent->SetLocationAtSplinePoint(index, Position, ESplineCoordinateSpace::World);
+
+}
+
+void ARetrogradePath::AddNewPoint()
+{
+	if(!OtherBody || !SplineComponent || !Mesh) return;
+
+	if(Timer >= Duration)
+	{
+		SplineComponent->RemoveSplinePoint(0); // Remove Oldest Spline Point
+		SplineMeshes[0]->DestroyComponent(); // Destroy Oldest Spline Mesh
+		SplineMeshes.RemoveAt(0); // Remove Oldest Spline Mesh
+	}
+	
+	// Get position of other body
+	FVector Position = OtherBody->GetActorLocation();
+	
+	// Add spline point at position of other body
+	SplineComponent->AddSplinePoint(Position, ESplineCoordinateSpace::World, true);	
+	
+	// Update counter
+	SplinePointCount = SplineComponent->GetNumberOfSplinePoints();
+
+}
+
---
+++ Source/Orbit_Simulation/Public/RetrogradePath.h
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SplineComponent.h"
+#include "Components/SplineMeshComponent.h"
+#include "GameFramework/Actor.h"
+#include "RetrogradePath.generated.h"
+
+class AAstroBody;
+class ASim;
+
+UCLASS()
+class ORBIT_SIMULATION_API ARetrogradePath : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ARetrogradePath();
+
+protected:
+	virtual void OnConstruction(const FTransform& Transform) override;
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void InitializeFirstPoints();
+
+	void AddSplineMesh();
+	void UpdateMesh();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	void UpdateLastPoint();
+	void AddNewPoint();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Astro")
+	ASim* Sim;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Astro")
+	AAstroBody* ReferenceBody;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Astro")
+	AAstroBody* OtherBody;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Time")
+	double Duration; // How long the path will be traced for (in Days)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Transient,  Category = "Time")
+	double Timer;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Time")
+	double Interval; // Time in Days between updates to the path
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Transient, Category = "Time")
+	double TimeSinceLastUpdate;
+	
+
+	
+	// Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	USceneComponent* SceneRoot;
+	// Spline
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spline")
+	USplineComponent* SplineComponent;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spline")
+	UStaticMesh* Mesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spline") // This should probably be a Queue
+	TArray<USplineMeshComponent*> SplineMeshes; // Array of Spline Meshes
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spline")
+	TEnumAsByte<ESplineMeshAxis::Type> ForwardAxis;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spline")
+	UMaterialInterface* DefaultMaterial;
+
+	// Counters
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Transient, Category = "Count")
+	int SplinePointCount;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Transient, Category = "Count")
+	int SplineMeshCount;
+};
